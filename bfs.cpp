ab962163a584aa27432d15acfb81dea6e3a66ff5
rahulnoobcoder <sidharth2197@gmail.com>
2025-07-20 11:26:57 +0530

    ijd

--- bfs.cpp
+++ bfs.cpp
@@ -17,24 +17,26 @@ int main()
         adj[y].push_back(x);
         // For undirected graph: adj[y].push_back(x);
     }
-
     
-
     vector <int> visited(n,0);
-    queue <int> q;
 
-    q.push(0);
+    queue<int> q;
+
+    q.push(4);
+
     while(!q.empty()){
         for(auto i:adj[q.front()]){
-            if(visited[i]==0)
-            q.push(i);
-            visited[i]=1;
+            if (!visited[i]){
+                q.push(i);
+                visited[i]=1;
+            }
         }
-
-        cout<<q.front()<<" ";
         visited[q.front()]=1;
+        cout<<q.front()<<" ";
         q.pop();
+
     }
+
 
     cout<<endl;
     
